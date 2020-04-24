057dd950946cb578f351ed92c8828bd25748b5cf
xiaoran <303176152@qq.com>
2020-04-24 10:15:28 +0800

    菜单

--- atcrowdfunding-main/src/main/java/com/hr/atcrowdfunding/controller/DispatcherController.java
+++ atcrowdfunding-main/src/main/java/com/hr/atcrowdfunding/controller/DispatcherController.java
@@ -1,6 +1,7 @@
 package com.hr.atcrowdfunding.controller;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpSession;
@@ -13,13 +14,18 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.hr.atcrowdfunding.bean.TAdmin;
+import com.hr.atcrowdfunding.bean.TMenu;
 import com.hr.atcrowdfunding.service.TAdminService;
+import com.hr.atcrowdfunding.service.TMenuService;
 import com.hr.atcrowdfunding.util.Const;
 
 @Controller
 public class DispatcherController {
 
 	Logger log = LoggerFactory.getLogger(DispatcherController.class);
+	
+	@Autowired
+	TMenuService menuService;
 	
 	@Autowired
 	TAdminService adminService;
@@ -36,6 +42,17 @@ public class DispatcherController {
 		
 		log.debug("跳转到登录页面...");
 		return "login";
+	}
+	
+	@RequestMapping("/main")
+	public String main(HttpSession session) {
+		
+		log.debug("跳转到主页面...");
+		
+		List<TMenu> menuList = menuService.listMenuAll();
+		session.setAttribute("menuList", menuList);
+		
+		return "main";
 	}
 	
 	@RequestMapping("/logout")
@@ -63,7 +80,8 @@ public class DispatcherController {
 			session.setAttribute(Const.LOGIN_ADMIN, admin);
 			
 			log.debug("登录成功...");
-			return "main";
+//			return "main"; //避免表单重复提交，采用重定向
+			return "redirect:/main";
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
---
+++ atcrowdfunding-manager-api/src/main/java/com/hr/atcrowdfunding/service/TMenuService.java
@@ -0,0 +1,11 @@
+package com.hr.atcrowdfunding.service;
+
+import java.util.List;
+
+import com.hr.atcrowdfunding.bean.TMenu;
+
+public interface TMenuService {
+
+	List<TMenu> listMenuAll();
+
+}
